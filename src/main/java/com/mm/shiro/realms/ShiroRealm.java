0109f684f95ccf3e3e323b3acdb50dde8b6720ab
shmilymin <83432087@qq.com>
2017-10-15 23:59:50 +0800

    add md5盐值加密

--- src/main/java/com/mm/shiro/realms/ShiroRealm.java
+++ src/main/java/com/mm/shiro/realms/ShiroRealm.java
@@ -3,6 +3,7 @@ package com.mm.shiro.realms;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.realm.AuthenticatingRealm;
+import org.apache.shiro.util.ByteSource;
 
 public class ShiroRealm extends AuthenticatingRealm {
 
@@ -21,10 +22,18 @@ public class ShiroRealm extends AuthenticatingRealm {
         // 1.principal: 认证实体信息
         Object principal = username;
         // 2.credentials: 密码
-        Object credentials = "e10adc3949ba59abbe56e057f20f883e";
+        Object credentials = null;//"fc1709d0a95a6be30bc5926fdb7f22f4";
+        if ("admin".equals(username)) {
+            credentials = "038bdaf98f2037b31f1e75b5b4c9b26e";
+        } else if ("user".equals(username)) {
+            credentials = "098d2c478e9c11555ce2823231e02ec1";
+        }
         // 3.realName: 当前realm对象的name。调用父类的getName()方法即可
         String realName = getName();
-        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(principal, credentials, realName);
+        // 4.盐值
+        ByteSource credentialsSalt = ByteSource.Util.bytes(username);
+        SimpleAuthenticationInfo info = null;//new SimpleAuthenticationInfo(principal, credentials, realName);
+        info = new SimpleAuthenticationInfo(principal, credentials, credentialsSalt, realName);
         return info;
     }
 
@@ -32,8 +41,8 @@ public class ShiroRealm extends AuthenticatingRealm {
     public static void main(String[] args) {
         String hashAlgorithmName = "MD5";
         Object credentials = "123456";
-        Object salt = null;
-        int hashIterations = 1;
+        Object salt = ByteSource.Util.bytes("user");
+        int hashIterations = 1024;
         Object result = new SimpleHash(hashAlgorithmName, credentials, salt, hashIterations);
         System.out.println(result);
     }
